2b3874067b4592e83febd24dc4b1475cef9e9c95
Gary Barkauski <gbarka2@gmail.com>
2021-02-05 14:36:16 -0700

    Add Leads and Lead JSX

---
+++ src/components/Lead.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Lead = (props) => {
+
+  return (
+    <div className="lead-div">
+
+    </div>
+  )
+}
+
+export default Lead
---
+++ src/components/Leads.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Leads = (props) => {
+
+  return (
+    <div className="leads-div">
+
+    </div>
+  )
+}
+
+export default Leads
