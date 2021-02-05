88c4363f9f28e1b3d8b85eb087865ee7a34303c6
Samantha Solomon <sleahsolomon@gmail.com>
2021-02-05 16:30:49 -0500

    added components and pages

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const About = (props) => {
+    return(
+        <div className="about-div">
+           About component     
+        </div>
+    )
+}
+
+export default About
