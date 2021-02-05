0889d041af9dea37f00fd814ec68ac9d7c57101a
Gary Barkauski <gbarka2@gmail.com>
2021-02-05 14:33:50 -0700

    Add Form JSX

---
+++ src/components/Form.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Form = (props) => {
+  
+
+  return (
+    <div className="form-div">
+      <form>
+        <input 
+          type="text"
+          name="companyName"
+          value={}
+          onChange={}  
+        />
+        <input 
+          type="text"
+          name="contactName"
+          value={}
+          onChange={}  
+        />
+          <input 
+            type="text"
+            name="contactTitle"
+            value={}
+            onChange={}  
+        />
+        <input 
+          type="text"
+          name="phone"
+          value={}
+          onChange={}  
+        />
+        <input 
+          type="text"
+          name="email"
+          value={}
+          onChange={}  
+        />
+        <input 
+          type="text"
+          name="estimatedRevenue"
+          value={}
+          onChange={}  
+        />
+        <input 
+          type="text"
+          name="companyName"
+          value={}
+          onChange={}  
+        />
+      </form>
+    </div>
+  )
+}
+
+export default Form
