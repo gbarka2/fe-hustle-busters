14bc43448540a26f2b6428e1854276381d8c1ec1
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-02-08 12:26:03 -0800

    removed excess

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
-import React from 'react'
+
 
 //Components & Pages
 import Nav from './components/Nav'
